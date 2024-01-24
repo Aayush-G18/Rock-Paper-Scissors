418e74652f7374e4cb5a65214ae32db3117069fc
Aayush-G18 <goyalaayush292@gmail.com>
2024-01-25 01:49:21 +0530

    Function for 1 round created

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 function getComputerChoice() {
-    let a=["Rock","Paper","Scissors"]
-    return (a[Math.floor(Math.random() * 3)])
+    let a = ["rock", "paper", "scissors"];
+    return a[Math.floor(Math.random() * 3)];
 }
+
+function playRound(player,computer) {
+    let playerSelection=player.toLowerCase();
+    let computerSelection=computer.toLowerCase();
+    let a = ["rock", "paper", "scissors"];
+    if (a.indexOf(playerSelection) === a.indexOf(computerSelection)) {
+        return `Tie Replay Round`;
+    } else {
+        switch (playerSelection) {
+            case "rock":
+                return computerSelection === "scissors"
+                ? `You Win!${playerSelection} beats ${computerSelection}`
+                : `You Lose!${computerSelection} beats ${playerSelection}`;
+                break;
+            case "paper":
+                return computerSelection === "rock"
+                ? `You Win!${playerSelection} beats ${computerSelection}`
+                : `You Lose!${computerSelection} beats ${playerSelection}`;
+                break;
+            case "scissors":
+                return computerSelection === "paper"
+                ? `You Win!${playerSelection} beats ${computerSelection}`
+                : `You Lose!${computerSelection} beats ${playerSelection}`;
+                break;
+            }
+        }
+    }
+var player="Rock";
+var comp=getComputerChoice();
+console.log(playRound(player,comp));
