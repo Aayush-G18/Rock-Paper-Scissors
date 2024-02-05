e2c03f712dc6f6376f1008d5fe11b62a4c8aee82
Aayush-G18 <goyalaayush292@gmail.com>
2024-02-05 20:03:23 +0530

    "clicking any of 3 buttons allows user i/p as rps can now play 1 round on 1 click result in console itself tho"

--- app.js
+++ app.js
@@ -4,30 +4,28 @@ function getComputerChoice() {
 }
 
 function playRound(player, computer) {
-    let playerSelection = player.toLowerCase();
-    let computerSelection = computer.toLowerCase();
 
-    if (playerSelection === computerSelection) {
+    if (player === computer) {
         console.log(`Tie! Replay Round`);
         return 0;
     } else {
-        switch (playerSelection) {
+        switch (player) {
             case "rock":
-                return computerSelection === "scissors" ? 1 : -1;
+                return computer === "scissors" ? 1 : -1;
             case "paper":
-                return computerSelection === "rock" ? 1 : -1;
+                return computer === "rock" ? 1 : -1;
             case "scissors":
-                return computerSelection === "paper" ? 1 : -1;
+                return computer === "paper" ? 1 : -1;
         }
     }
 }
 
-function game() {
+function game(player) {
 
     let scoreP=0;
     let scoreC=0;
     // for (let index = 0; index < 4; index++) {
-        var player=prompt("Enter value:");
+        // var player=prompt("Enter value:");
         var comp=getComputerChoice();
         let point=playRound(player,comp);
         switch (point) {
@@ -48,7 +46,9 @@ function game() {
         console.log(`You Lose ${scoreP}-${scoreC} computer`);
     }
 }
-const btns = document.body.querySelectorAll('#btn');
-btns.forEach(btn =>{
-    btn.addEventListener('click',game);
-});
+const btn1 = document.body.querySelector('#btn1');
+btn1.addEventListener('click',function() {game("rock")});
+const btn2 = document.body.querySelector('#btn2');
+btn2.addEventListener('click',function() {game("paper")});
+const btn3 = document.body.querySelector('#btn3');
+btn3.addEventListener('click',function() {game("scissors")});
